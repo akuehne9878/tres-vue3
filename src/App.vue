ea64ab58e2b5934a4a64fe31a85da42ee01b6503
Andreas Kühne <akuehne9878@gmail.com>
2024-12-21 11:02:44 +0100

    explosion view test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <TresCanvas window-size preset="flat">
-    <OrbitControls />
-    <TresPerspectiveCamera :position="[1200, 1000, 1500]" :look-at="[0, 400, 250]" :far="5000" />
-    <TresAmbientLight intensity="0.7" />
-    <TresDirectionalLight :position="[1000, 1000, 500]" intensity="0.8" />
+    <TresCanvas window-size preset="flat">
+      <OrbitControls />
+      <TresPerspectiveCamera :position="[1200, 1000, 1500]" :look-at="[0, 400, 250]" :far="5000" />
+      <TresAmbientLight intensity="0.7" />
+      <TresDirectionalLight :position="[1000, 1000, 500]" intensity="0.8" />
 
-    <template v-for="part in parts" :key="part.id">
-      <TresMesh :position="computePosition(part.position)" :rotation="degreesToRadians(part.rotation)">
-        <TresBoxGeometry :args="computeDimensions(part.part)" />
-        <TresMeshStandardMaterial color="#D2B48C" />
-      </TresMesh>
-      <TresLineSegments :geometry="getEdgesGeometry(computeDimensions(part.part))" :material="lineMaterial"
-        :position="computePosition(part.position)" :rotation="degreesToRadians(part.rotation)" />
-    </template>
+      <template v-for="part in parts" :key="part.id">
+        <TresMesh :position="computePosition(part.position)" :rotation="degreesToRadians(part.rotation)">
+          <TresBoxGeometry :args="computeDimensions(part.part)" />
+          <TresMeshStandardMaterial color="#D2B48C" />
+        </TresMesh>
+        <TresLineSegments :geometry="getEdgesGeometry(computeDimensions(part.part))" :material="lineMaterial"
+          :position="computePosition(part.position)" :rotation="degreesToRadians(part.rotation)" />
+      </template>
 
-    <TresAxesHelper :args="[1000]" />
-  </TresCanvas>
+      <TresAxesHelper :args="[1000]" />
+    </TresCanvas>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import { BoxGeometry, EdgesGeometry, LineBasicMaterial } from 'three';
+import { BoxGeometry, BufferGeometry, EdgesGeometry, Float32BufferAttribute, LineBasicMaterial, Vector3 } from 'three';
 import { TresCanvas } from '@tresjs/core';
 import { OrbitControls } from '@tresjs/cientos';
 
 interface Parameter {
   name: string;
-  value: number;
+  value: string | number;
   unit: string;
 }
 
@@ -41,6 +41,7 @@ interface Assembly {
   part: string;
   position: (number | string)[];
   rotation: number[];
+  measurements: number[];
 }
 
 interface Config {
@@ -54,7 +55,7 @@ const partsConfig = ref<Record<string, Part>>({});
 const parameters = ref<Parameter[]>([]);
 
 const loadParts = async () => {
-  const response = await fetch('/src/components.json');
+  const response = await fetch('/src/components_explosion.json');
   const data: Config = await response.json();
   parameters.value = data.parameters;
   partsConfig.value = data.parts;
@@ -65,26 +66,28 @@ const parametersMap = ref<Record<string, number>>({});
 
 const createParametersMap = () => {
   parameters.value.forEach(param => {
-    parametersMap.value[param.name] = param.value;
+    parametersMap.value[param.name] = eval(replaceParameters(param.value.toString()));
   });
 };
 
 const replaceParameters = (str: string) => {
   const regex = /\$\{(\w+)\}/g;
-  return str.replace(regex, (_, key) => parametersMap.value[key]?.toString() || '0');
+  return str.replace(regex, (_, key) => {
+    return parametersMap.value[key]?.toString() || '0';
+  });
 };
 
 const computeDimensions = (partName: string): [number, number, number] => {
   const part = partsConfig.value[partName];
   return [
-    parseFloat(replaceParameters(part.length)),
-    parseFloat(replaceParameters(part.width)),
-    parseFloat(replaceParameters(part.thickness))
+    eval(replaceParameters(part.length)),
+    eval(replaceParameters(part.width)),
+    eval(replaceParameters(part.thickness))
   ];
 };
 
 const computePosition = (pos: (number | string)[]): [number, number, number] => {
-  return pos.map(p => 
+  return pos.map(p =>
     typeof p === 'string' ? eval(replaceParameters(p)) : p
   ) as [number, number, number];
 };
